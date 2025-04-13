3f25f6c6ba8b57915d3fef6d105acf46d82c5a49
diego <diegoboliveira@gmail.com>
2025-04-13 17:59:01 -0300

    test: add unit tests for SaleItemPositivePriceSpecificationTests
    - Implemented tests for valid and invalid price validation in SaleItemPositivePriceSpecificationTests.
    - Covered positive and negative price scenarios to ensure proper validation.
    - Ensured the specification returns true for valid prices and false for invalid ones.

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Specifications/PositivePriceSpecification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ambev.DeveloperEvaluation.Domain.Aggregates;
+
+namespace Ambev.DeveloperEvaluation.Domain.Specifications
+{
+    public class PositivePriceSpecification : ISpecification<SaleItem>
+    {
+        public bool IsSatisfiedBy(SaleItem saleItem)
+        {
+            return saleItem.Product.Price > 0;
+        }
+    }
+}
